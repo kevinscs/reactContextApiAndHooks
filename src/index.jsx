f3f89d619c642afee5ce57b9321901af2b147214
kevinscs <kevinsatiro123@gmail.com>
2023-07-12 11:48:34 -0300

    Finalizando

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Home } from './templates/App/index';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 ReactDOM.render(
-      <Home />,
+  <React.StrictMode>
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
